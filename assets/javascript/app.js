56a4defd3bcf76d2a8b6acf02b9337ad9aa91e5b
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-09-11 15:01:39 -0500

    added time conversion variables to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,14 +25,14 @@
 
     // variables to grab users entry from form fields
     var trainNameForm = $("#trainNameForm").val().trim();
-    var destionationForm = $("#destinationForm").val().trim();
+    var destinationForm = $("#destinationForm").val().trim();
     var trainTimeForm = moment ($("#trainTimeForm").val().trim(), "HH:mm").format("HH:mm");
     var frequencyForm = $("#frequencyForm").val().trim();
 
     // variable to hold inputs
     var newTrain = {
         train: trainNameForm,
-        destination: destionationForm,
+        destination: destinationForm,
         first: trainTimeForm,
         frequency: frequencyForm
     };
@@ -45,4 +45,41 @@
     console.log(newTrain.first);
     console.log(newTrain.frequency);
 
+    //clear input form fields
+    $("#trainNameForm").val("");
+    $("#destinationForm").val("");
+    $("#trainTimeForm").val("");
+    $("#frequencyForm").val("");
+
   });
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+    console.log(childSnapshot.val());
+
+    // store the snapshots in there own variable
+    var trainName = childSnapshot.val().train;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().first;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // variable for conversion of time
+    var trainTimeConverted = moment(trainTime, "HH:mm");
+    console.log(trainTimeConverted);
+
+    // variable for difference in time
+    var timeDifference = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log(timeDifference);
+
+    // variable for frequency conversion
+    var frequencyMinutes = childSnapshot.val().frequency;
+    console.log("What is the frequency in minutes: " + frequencyMinutes);
+
+    // variable for the number of minutes away conversion
+    var minutesAway = Math.abs(timeDifference % frequencyMinutes);
+    console.log("The train is this many minutes away: " + minutesAway);
+
+    // variable for converting when the next arrival will be
+    var nextArrival = moment(currentTime).add(minutesAway, "minutes").format("hh:mm A");
+    console.log("The next arriving train is: " + nextArrival);
+  })
